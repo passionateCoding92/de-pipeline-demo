2df2e83d32a4b1cc334bc2da30b55567fdc94d0b
BVK <passionatecoding1992@gmail.com>
2026-04-24 18:17:49 +0530

    Added tax column after discount calculation

--- pipeline.py
+++ pipeline.py
@@ -1,5 +1,6 @@
 from pyspark.sql.functions import col
 
 def transform(df):
-
-    return df.withColumn("discounted_price", col("price") * 0.9)
+    df = df.withColumn("discounted_price", col("price") * 0.9)
+    df = df.withColumn("tax", col("discounted_price") * 0.18)
+    return df
